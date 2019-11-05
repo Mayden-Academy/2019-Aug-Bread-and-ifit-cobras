9ae5df396144018d37a01f0fe3876516dc8172b9
John Hasznosi <noctiscreo@gmail.com>
2019-11-05 11:39:29 +0000

    Carousel added with three images.

--- app/index.php
+++ app/index.php
@@ -65,7 +65,27 @@
     </form>
 </aside>
 <main class="col-9">
-    blah
+    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="../res/images/toast1.jpg" class="d-block w-100" alt="Toast!">
+            </div>
+            <div class="carousel-item">
+                <img src="../res/images/toast2.jpg" class="d-block w-100" alt="MORE toast!">
+            </div>
+            <div class="carousel-item">
+                <img src="../res/images/toast3.jpg" class="d-block w-100" alt="EVEN MORE toast!">
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
 </main>
 </div>
 </body>

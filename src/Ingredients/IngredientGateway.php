0c4be1547ef23cba4a6f482583fe5f5dfeb743f3
Jason Rees <iwhisperit@gmail.com>
2019-11-05 11:23:07 +0000

    changed paramter name from array to ingredients

--- src/Ingredients/IngredientGateway.php
+++ src/Ingredients/IngredientGateway.php
@@ -6,14 +6,33 @@ namespace BreadAndIfit\Ingredients;
 
 class IngredientGateway
 {
-    static private function formatData(array $array) {
+    static private function formatData(array $ingredients): string
+    {
         $url = "http://www.recipepuppy.com/api/?i=";
-        foreach ($array as $ingredient) {
+        foreach ($ingredients as $ingredient) {
             $url .= $ingredient;
             $url .= ',';
         }
-        $url = substr($url,0,-1);
+        $url = substr($url, 0, -1);
         return $url;
     }
 
+    static private function sendDataToAPI($url)
+    {
+
+
+    }
+
+
+
+
+
+
+
+
+
+    public function sendDataReturnResponse()
+    {
+
+    }
 }

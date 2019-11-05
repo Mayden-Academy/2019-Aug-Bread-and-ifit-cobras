bdc15067bf789e1e8b49b2ba0853f09c8d9c2e00
John Hasznosi <noctiscreo@gmail.com>
2019-11-05 11:14:09 +0000

    "Added accordions and submit button."

--- app/index.php
+++ app/index.php
@@ -13,10 +13,60 @@
     <a class="col-6 flex-sm-fill text-sm-center nav-link" href="#">Bread and Ifits</a>
     <a class="col-3 flex-sm-fill text-sm-center nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Recipe Book</a>
 </nav>
-
+<div class="row">
 <aside class="col-3">
-    This is sorted
-</aside>
+    <form>
+        <div class="accordion" id="accordionExample">
+            <div class="card">
+                <div class="card-header" id="headingOne">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Collapsible Group Item #1
+                        </button>
+                    </h2>
+                </div>
 
+                <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                    <div class="card-body">
+                        Sample text.
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingTwo">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Collapsible Group Item #2
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                    <div class="card-body">
+                        Sample text 2.
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-header" id="headingThree">
+                    <h2 class="mb-0">
+                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Collapsible Group Item #3
+                        </button>
+                    </h2>
+                </div>
+                <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                    <div class="card-body">
+                        Sample text 3.
+                    </div>
+                </div>
+            </div>
+        </div>
+        <input type="submit" value="Get Recipe">
+    </form>
+</aside>
+<main class="col-9">
+    blah
+</main>
+</div>
 </body>
 </html>

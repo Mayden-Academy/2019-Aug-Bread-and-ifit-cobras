5d8dd07850cf5168d5575362f8ef70456974030e
John Hasznosi <noctiscreo@gmail.com>
2019-11-05 10:30:29 +0000

    Added navbar.

--- app/index.php
+++ app/index.php
@@ -3,10 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style.css" >
     <script type="text/javascript" src="js/dist/main.js"></script>
 </head>
 <body>
-
+<nav class="nav nav-pills flex-column flex-sm-row">
+    <a class="col-3 flex-sm-fill text-sm-center nav-link active" href="#">Active</a>
+    <a class="col-6 flex-sm-fill text-sm-center nav-link" href="#">Longer nav link</a>
+    <a class="col-3 flex-sm-fill text-sm-center nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+</nav>
 </body>
 </html>

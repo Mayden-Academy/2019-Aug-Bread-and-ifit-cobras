2aeeb3c928871563c3b26bb47deed808d53e0659
John Hasznosi <noctiscreo@gmail.com>
2019-11-06 10:45:08 +0000

    added functionality to the carousel

--- app/index.php
+++ app/index.php
@@ -7,6 +7,9 @@
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link href="https://fonts.googleapis.com/css?family=Raleway&display=swap" rel="stylesheet">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script type="text/javascript" src="js/dist/main.js"></script>
 </head>
 <body>
@@ -82,16 +85,16 @@
 
 
     <main class="col-9">
-        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="5000">
             <div class="carousel-inner">
                 <div class="carousel-item">
                     <img src="../res/images/toast1.jpg" class="d-block w-100" alt="Toast!">
                 </div>
                 <div class="carousel-item active">
-                    <img src="../res/images/toast2.jpg" class="d-block w-100" alt="MORE toast!">
+                    <img src="../res/images/ifit1.jpg" class="d-block w-100" alt="half empty cupboard">
                 </div>
                 <div class="carousel-item">
-                    <img src="../res/images/toast3.jpg" class="d-block w-100" alt="EVEN MORE toast!">
+                    <img src="../res/images/toast2.jpg" class="d-block w-100" alt="EVEN MORE toast!">
                 </div>
             </div>
             <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">

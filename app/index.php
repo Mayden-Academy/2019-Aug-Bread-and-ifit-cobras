dbc1f8916abbc71ed36eae0ed915d31493c90ea9
Georgia Connell-Bass <georgiaconnellwork@gmail.com>
2019-11-07 10:40:38 +0000

    trying if statement with  superglobal to replace carousel. Not yet fixed on page refresh

--- app/index.php
+++ app/index.php
@@ -21,128 +21,133 @@
 </nav>
 <div class="nav-transform"></div>
 
-<div class="container-fluid">
-<div class="row" id="mainContent">
-    <aside class="col-3 justify-content-center">
-        <form>
-            <input class="choiceBtn " id="getReciepeBtn" type="submit" value="Get Recipe"><br>
-            <input class="choiceBtn" id="expandAll" type="submit" value="Expand All">
+    <div class="container-fluid">
+        <div class="row" id="mainContent">
+            <aside class="col-3 justify-content-center">
+                <form>
+                    <input class="choiceBtn " id="getReciepeBtn" type="submit" value="Get Recipe"><br>
+                    <input class="choiceBtn" id="expandAll" type="submit" value="Expand All">
 
 
-            <div class="accordion " id="accordionExample">
-                <div class="card">
-                    <div class="card-header" id="headingOne">
-                        <h2 class="mb-0">
-                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
-                                    aria-expanded="true" aria-controls="collapseOne">
-                                Vegetables
-                            </button>
-                        </h2>
-                    </div>
+                    <div class="accordion " id="accordionExample">
+                        <div class="card">
+                            <div class="card-header" id="headingOne">
+                                <h2 class="mb-0">
+                                    <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne"
+                                            aria-expanded="true" aria-controls="collapseOne">
+                                        Vegetables
+                                    </button>
+                                </h2>
+                            </div>
 
-                    <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
-                         data-parent="#accordionExample">
-                        <div class="card-body">
-                            <input type="checkbox" value="tomato" name="tomato"><span class="ingredient"> tomato</span>
+                            <div id="collapseOne" class="collapse show" aria-labelledby="headingOne"
+                                 data-parent="#accordionExample">
+                                <div class="card-body">
+                                    <input type="checkbox" value="tomato" name="tomato"><span class="ingredient"> tomato</span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="headingTwo">
+                                <h2 class="mb-0">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                            data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                        Dairy
+                                    </button>
+                                </h2>
+                            </div>
+                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                                <div class="card-body">
+                                    Sample text 2.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="headingThree">
+                                <h2 class="mb-0">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
+                                            data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                        Meat
+                                    </button>
+                                </h2>
+                            </div>
+                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
+                                 data-parent="#accordionExample">
+                                <div class="card-body">
+                                    Sample text 3.
+                                </div>
+
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="card">
-                    <div class="card-header" id="headingTwo">
-                        <h2 class="mb-0">
-                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
-                                    data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                Dairy
-                            </button>
-                        </h2>
-                    </div>
-                    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
-                        <div class="card-body">
-                            Sample text 2.
+                </form>
+                <form method="post">
+                    <input value="checked" type="checkbox" name="check">
+                    <input type="submit">
+                </form>
+            </aside>
+
+
+            <main class="col-9">
+                <?php
+                if (!empty($_POST)[]){
+                    echo ' <div id="mainPannel">
+                    <div class="row recipe">
+                        <div class="col-10 col-lg-4">
+                            <img src="../res/images/food.jpg" alt="...">
+                        </div>
+                        <div class="col-10 col-lg-8">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cupiditate debitis ea eaque, hic ipsum magnam, nesciunt nobis nulla, quas quibusdam sed vitae voluptate. Autem excepturi laudantium necessitatibus neque rem.</p>
+                            <a id="linkToRecipeBtn" href="#" class="btn btn-primary float-right">Go somewhere</a>
                         </div>
                     </div>
-                </div>
-                <div class="card">
-                    <div class="card-header" id="headingThree">
-                        <h2 class="mb-0">
-                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse"
-                                    data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                Meat
-                            </button>
-                        </h2>
-                    </div>
-                    <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
-                         data-parent="#accordionExample">
-                        <div class="card-body">
-                            Sample text 3.
+                    <div class="row recipe">
+                        <div class="col-10 col-lg-4">
+                            <img src="../res/images/food.jpg" alt="...">
+                        </div>
+                        <div class="col-10 col-lg-8">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cupiditate debitis ea eaque, hic ipsum magnam, nesciunt nobis nulla, quas quibusdam sed vitae voluptate. Autem excepturi laudantium necessitatibus neque rem.</p>
+                            <a id="linkToRecipeBtn" href="#" class="btn btn-primary float-right">Go somewhere</a>
                         </div>
                     </div>
-                </div>
-            </div>
-
-        </form>
-    </aside>
-
-
-    <main class="col-9">
-        <div id="mainPannel">
-            <div class="row recipe">
-                <div class="col-10 col-lg-4">
-                    <img src="../res/images/food.jpg" alt="...">
-                </div>
-                <div class="col-10 col-lg-8">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cupiditate debitis ea eaque, hic ipsum magnam, nesciunt nobis nulla, quas quibusdam sed vitae voluptate. Autem excepturi laudantium necessitatibus neque rem.</p>
-                    <a id="linkToRecipeBtn" href="#" class="btn btn-primary float-right">Go somewhere</a>
-                </div>
-            </div>
-            <div class="row recipe">
-                <div class="col-10 col-lg-4">
-                    <img src="../res/images/food.jpg" alt="...">
-                </div>
-                <div class="col-10 col-lg-8">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cupiditate debitis ea eaque, hic ipsum magnam, nesciunt nobis nulla, quas quibusdam sed vitae voluptate. Autem excepturi laudantium necessitatibus neque rem.</p>
-                    <a id="linkToRecipeBtn" href="#" class="btn btn-primary float-right">Go somewhere</a>
-                </div>
-            </div>
-
-            <div class="row recipe">
-                <div class="col-10 col-lg-4">
-                    <img src="../res/images/food.jpg" alt="...">
-                </div>
-                <div class="col-10 col-lg-8">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cupiditate debitis ea eaque, hic ipsum magnam, nesciunt nobis nulla, quas quibusdam sed vitae voluptate. Autem excepturi laudantium necessitatibus neque rem.</p>
-                    <a id="linkToRecipeBtn" href="#" class="btn btn-primary float-right">Go somewhere</a>
-                </div>
-            </div>
+        
+                    <div class="row recipe">
+                        <div class="col-10 col-lg-4">
+                            <img src="../res/images/food.jpg" alt="...">
+                        </div>
+                        <div class="col-10 col-lg-8">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cupiditate debitis ea eaque, hic ipsum magnam, nesciunt nobis nulla, quas quibusdam sed vitae voluptate. Autem excepturi laudantium necessitatibus neque rem.</p>
+                            <a id="linkToRecipeBtn" href="#" class="btn btn-primary float-right">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>';
+                }else {
+                    echo '<div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="5000">
+                    <div class="carousel-inner">
+                        <div class="carousel-item">
+                            <img src="../res/images/toast1.jpg" class="d-block w-100" alt="Toast!">
+                        </div>
+                        <div class="carousel-item active">
+                            <img src="../res/images/ifit1.jpg" class="d-block w-100" alt="half empty cupboard">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="../res/images/toast2.jpg" class="d-block w-100" alt="EVEN MORE toast!">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>';} ?>
+            </main>
         </div>
-
-
-<!--        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="5000">-->
-<!--            <div class="carousel-inner">-->
-<!--                <div class="carousel-item">-->
-<!--                    <img src="../res/images/toast1.jpg" class="d-block w-100" alt="Toast!">-->
-<!--                </div>-->
-<!--                <div class="carousel-item active">-->
-<!--                    <img src="../res/images/ifit1.jpg" class="d-block w-100" alt="half empty cupboard">-->
-<!--                </div>-->
-<!--                <div class="carousel-item">-->
-<!--                    <img src="../res/images/toast2.jpg" class="d-block w-100" alt="EVEN MORE toast!">-->
-<!--                </div>-->
-<!--            </div>-->
-<!--            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">-->
-<!--                <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
-<!--                <span class="sr-only">Previous</span>-->
-<!--            </a>-->
-<!--            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">-->
-<!--                <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
-<!--                <span class="sr-only">Next</span>-->
-<!--            </a>-->
-<!--        </div>-->
-    </main>
-</div>
-</div>
+    </div>
 </body>
 </html>

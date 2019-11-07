2901c0a963a4980cc6fcf6968e15efbbed9b2dc2
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-11-07 12:23:12 +0000

    added recipe template

--- app/index.php
+++ app/index.php
@@ -50,7 +50,26 @@ use BreadAndIfit\Ingredients\IngredientHydrator;
             <input class="choice-btn col-2" id="getRecipeBtn" type="submit" value="Get Recipe">
         </aside>
         <main class="col-10">
-            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="5000">
+
+            <?php
+            if (!empty($_POST)){
+                echo '<div id="mainPannel">
+                    <div class="row recipe mx-auto">
+                        <div class="col-10 col-lg-4">
+                            <img src="../res/images/food.jpg" alt="...">
+                        </div>
+                        <div class="col-10 col-lg-8">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card\'s content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque cupiditate debitis ea eaque, hic ipsum magnam, nesciunt nobis nulla, quas quibusdam sed vitae voluptate. Autem excepturi laudantium necessitatibus neque rem.</p>
+                            <a id="linkToRecipeBtn" href="#" class="btn btn-primary float-right">Go somewhere</a>
+                        </div>
+                    </div>
+                    
+                    
+                </div>';
+            } else {
+                echo '
+                 <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="5000">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
                         <img src="../res/images/toast1.jpg" class="d-block w-100" alt="Toast!">
@@ -71,86 +90,9 @@ use BreadAndIfit\Ingredients\IngredientHydrator;
                     <span class="sr-only">Next</span>
                 </a>
             </div>
-        </main>
-
-
-        <main class="col-9">
-            <?php
-            require('../vendor/autoload.php');
-            $data = \BreadAndIfit\Ingredients\IngredientGateway::sendDataToAPI('http://www.recipepuppy.com/api/?i=tomato');
-
-            $data = json_decode($data);
-
-            var_dump($data->results[0]);
-
+                ';
+            }
             ?>
-            <div id="mainPannel">
-
-                <div class="row recipe">
-                    <div class="col-10 col-lg-4">
-                        <img src="../res/images/food.jpg" alt="...">
-                    </div>
-                    <div class="col-10 col-lg-8">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                            of the card's content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
-                            cupiditate debitis ea eaque, hic ipsum magnam, nesciunt nobis nulla, quas quibusdam sed
-                            vitae voluptate. Autem excepturi laudantium necessitatibus neque rem.</p>
-                        <a id="linkToRecipeBtn" href="#" class="btn btn-primary float-right">Go somewhere</a>
-                    </div>
-                </div>
-
-                <div class="row recipe">
-                    <div class="col-10 col-lg-4">
-                        <img src="../res/images/food.jpg" alt="...">
-                    </div>
-                    <div class="col-10 col-lg-8">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                            of the card's content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
-                            cupiditate debitis ea eaque, hic ipsum magnam, nesciunt nobis nulla, quas quibusdam sed
-                            vitae voluptate. Autem excepturi laudantium necessitatibus neque rem.</p>
-                        <a id="linkToRecipeBtn" href="#" class="btn btn-primary float-right">Go somewhere</a>
-                    </div>
-                </div>
-
-                <div class="row recipe">
-                    <div class="col-10 col-lg-4">
-                        <img src="../res/images/food.jpg" alt="...">
-                    </div>
-                    <div class="col-10 col-lg-8">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk
-                            of the card's content. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque
-                            cupiditate debitis ea eaque, hic ipsum magnam, nesciunt nobis nulla, quas quibusdam sed
-                            vitae voluptate. Autem excepturi laudantium necessitatibus neque rem.</p>
-                        <a id="linkToRecipeBtn" href="#" class="btn btn-primary float-right">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-
-
-            <!--        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="5000">-->
-            <!--            <div class="carousel-inner">-->
-            <!--                <div class="carousel-item">-->
-            <!--                    <img src="../res/images/toast1.jpg" class="d-block w-100" alt="Toast!">-->
-            <!--                </div>-->
-            <!--                <div class="carousel-item active">-->
-            <!--                    <img src="../res/images/ifit1.jpg" class="d-block w-100" alt="half empty cupboard">-->
-            <!--                </div>-->
-            <!--                <div class="carousel-item">-->
-            <!--                    <img src="../res/images/toast2.jpg" class="d-block w-100" alt="EVEN MORE toast!">-->
-            <!--                </div>-->
-            <!--            </div>-->
-            <!--            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">-->
-            <!--                <span class="carousel-control-prev-icon" aria-hidden="true"></span>-->
-            <!--                <span class="sr-only">Previous</span>-->
-            <!--            </a>-->
-            <!--            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">-->
-            <!--                <span class="carousel-control-next-icon" aria-hidden="true"></span>-->
-            <!--                <span class="sr-only">Next</span>-->
-            <!--            </a>-->
-            <!--        </div>-->
         </main>
     </div>
 </div>

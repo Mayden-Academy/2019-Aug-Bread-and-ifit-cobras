0a9f7e344fde5b7a857c0c2ec717b5b4ccd1f45c
Daniel Gardiner <gardiner.daniel@hotmail.co.uk>
2019-11-07 16:33:20 +0000

    fixed ingredientvalidator test

--- tests/Ingredients/IngredientValidatorTest.php
+++ tests/Ingredients/IngredientValidatorTest.php
@@ -9,13 +9,13 @@ class IngredientValidatorTest extends TestCase
 {
     public function testCheckUserInput_success()
     {
-        $result = IngredientValidator::checkUserInput(['check_list' => ['onions', 'carrots']]);
+        $result = IngredientValidator::checkUserInput(['onions', 'carrots']);
         $this->assertEquals($result, true);
     }
 
     public function testCheckUserInput_failure()
     {
-        $result = IngredientValidator::checkUserInput(['check_list' => ['oni ons', 'carrots']]);
+        $result = IngredientValidator::checkUserInput(['oni ons', 'carrots']);
         $this->assertEquals($result, false);
     }
 }
